42b688293c3f4605c812acf2e4745d9d47234c52
Siddharth Pathak <67097391+sid993100@users.noreply.github.com>
2023-02-22 21:17:10 +0530

    Storing Data into the DB Using Mongoose Model

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 //Available Routes
 // app.get('/', (req, res) => {
 //   res.send('Hello Siddharth!')
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const {Schema} = mongoose;
 
-const UserSchema = new ({
+const UserSchema = new Schema ({
     name:{
         type:String,
         required: true
