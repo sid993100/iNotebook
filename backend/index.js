451c3e73300ec6638963b748914efa34febca235
Siddharth Pathak <67097391+sid993100@users.noreply.github.com>
2023-02-25 17:08:03 +0530

    fetching all notes

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,23 @@
-const { connect } = require('mongoose');
-const connectToMongo = require('./db');
+const { connect } = require("mongoose");
+const connectToMongo = require("./db");
 
 connectToMongo();
 
 //this line get get from express js--no need to learn
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
 
 //Available Routes
 // app.get('/', (req, res) => {
 //   res.send('Hello Siddharth!')
 // })
 
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Url - http://localhost:${port}`)
-})
+  console.log(`Url - http://localhost:${port}`);
+});
