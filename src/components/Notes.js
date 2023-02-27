6ac2517e8929877e04bfc4b6014806e06e6f5345
Siddharth Pathak <67097391+sid993100@users.noreply.github.com>
2023-02-27 23:56:04 +0530

    font icons

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,18 +1,17 @@
 import React, { useContext } from "react";
 import Noteitem from "./Noteitem";
-
 import noteContext from "../context/notes/noteContext";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, setNotes } = context;
+  const { notes } = context;
 
   return (
     <div className="row my-3">
       <h1>Your Notes</h1>
 
       {notes.map((note) => {
-        return <Noteitem note={note}/>
+        return <Noteitem key={note._id} note={note}/>
       })}
     </div>
   );
